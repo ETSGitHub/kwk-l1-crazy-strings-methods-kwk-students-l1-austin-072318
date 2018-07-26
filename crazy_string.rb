e5a555a7e92fdca5805ef1dc3ebc81cfe2aae2c7
ETSGitHub <etschulte@gmail.com>
2018-07-26 21:32:36 +0000

    automatically backed up by learn

--- crazy_string.rb
+++ crazy_string.rb
@@ -4,7 +4,12 @@ def crazy_strings(a,b)
   
   new_a = a.reverse.upcase
   
+<<<<<<< HEAD
   new_b = b.swapcase.gsub("s","z")
   
   return new_a + " " + new_b
+=======
+  new_b = b.swapcase.gsub!("s","z")
+  return new_a + new_b
+>>>>>>> 0c25664f9a0ddea67a74638e0a742d1b625a039d
 end
